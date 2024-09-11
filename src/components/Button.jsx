8fa3816ec4cb709e253854e7e086b46266c47b80
Garik Asatryan <asa7ur@gmail.com>
2024-09-11 17:01:42 +0200

    some changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,7 +12,7 @@ const Button = ({ icon: Icon, url }) => {
 
   return (
     <Wrapper onClick={handleClick} className={isActive ? 'highlighted' : ''}>
-      <div className='styled-circle'>
+      <div className='btn'>
         <Icon className='icon' />
       </div>
     </Wrapper>
@@ -28,36 +28,6 @@ const Wrapper = styled.div`
     font-size: 1.25rem;
   }
 
-  .styled-circle {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background: radial-gradient(circle at 35% 30%, #374151, #030712);
-    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.25),
-      0px 0px 20px rgba(0, 0, 0, 0.25);
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: background-color 0.3s ease, color 0.3s ease;
-  }
-
-  .styled-circle::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    background: radial-gradient(
-      circle at 38% 35%,
-      rgba(55, 65, 81, 0.5),
-      rgba(3, 7, 18, 0.5)
-    );
-    border: 1.5px solid rgba(255, 255, 255, 0.5);
-    mix-blend-mode: overlay;
-    pointer-events: none;
-  }
+  
 
 `
--- src/pages/Vehiculo.jsx
+++ src/pages/Vehiculo.jsx
@@ -1,6 +1,33 @@
-const Coche = () => {
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { NavbarBottom } from '../components'
+
+const Vehiculo = () => {
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div>Coche</div>
+    <Wrapper style={{ height: `${viewportHeight}px` }}>
+      <NavbarBottom />
+    </Wrapper>
   )
 }
-export default Coche
+export default Vehiculo
+
+const Wrapper = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+`
